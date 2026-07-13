acfceec0d5ee4db125757c44a7bd327585eb06b8
kanjigoel <kanjigoil327@gmail.com>
2026-07-13 11:50:01 +0500

    Fix database to use Railway Postgres

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+import os
+
 class Config:
-    SECRET_KEY = "dar_ul_sukun_secret_key"
-
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/dar_ul_sukun_m"
-
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'dar_ul_sukun_secret_key')
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
